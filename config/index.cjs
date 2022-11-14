17989910a86dc519cf2c063fc95ca3c4cde53a76
Agnoy1222 <86405386+Agnoy1222@users.noreply.github.com>
2022-11-14 20:45:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -65,7 +65,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oB1QI6svRoAfQeKWOA4_TrWmU6Bw',
+      id: 'oB1QI6qmeGocZZYNIEe_9KwXiJaM',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'qMveGdWgzkE6RKQ8wnldif05n9ItfM4ax3f2pHD_CM4',
       // 所在省份
